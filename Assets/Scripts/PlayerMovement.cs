0a251c1004fb38e853bf7ea1d8b5ad3acdd3ad1b
DrGass <80747033+DrGass@users.noreply.github.com>
2023-02-06 19:55:21 +0100

    2d character with moving and jumping

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpingForce;
+    public LayerMask groundObjects;
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+    public float checkRadius;
+    public int maxJumpCount;
+    
+    private Rigidbody2D rb;
+    private float moveDirection;
+    private bool facingRight = true;
+    private bool isJumping;
+    private bool isGrounded;
+    private float jumpCount;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpCount = maxJumpCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Inputs
+        ProcessInputs();
+        
+        // Animate
+        Animate();
+
+    }
+    private void FixedUpdate()
+    {
+        //Check if grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+        if (isGrounded)
+        {
+            jumpCount = maxJumpCount;
+        }
+        
+        // Move
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f,jumpingForce));
+            jumpCount--;
+        }
+        isJumping = false;
+    }
+
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); // Scale of -1 -> 1.
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f,180f,0f);
+    }
+    
+}
